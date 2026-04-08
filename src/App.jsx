b31030512899e887f158d6f5782a7318f58c4f92
briankibiku <kibikukimani2@gmail.com>
2026-04-08 11:57:21 +0300

    clean landing page

--- src/App.jsx
+++ src/App.jsx
@@ -308,8 +308,10 @@ function Hero() {
       <canvas
         ref={canvasRef}
         style={{
-          position: 'absolute', inset: 0,
-          width: '100%', height: '100%',
+          position: 'absolute',
+          // right: 0,
+          inset: 0,
+          width: '20%', height: '100%',
           pointerEvents: 'none', zIndex: 0,
         }}
       />
